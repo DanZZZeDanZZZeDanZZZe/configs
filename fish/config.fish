8736c1fa2fbc55b18236e5ce9a06ab8afa0b2187
Artyom <jyjyjyj3@gmail.com>
2022-06-14 16:08:12 +0300

    fish: update aliases

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,6 @@ export PATH="$HOME/.cargo/bin:$PATH"
 set -gx EDITOR vim
 
 alias v="nvim"
-alias vi="nvim"
-alias vim="nvim"
 
 function fish_prompt
     echo -n (whoami): (prompt_pwd) (fish_vcs_prompt) '$ '
